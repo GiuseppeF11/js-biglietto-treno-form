82ea48453661b11f8e701c71e77c7a7f77f0f412
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-04 15:52:34 +0100

    Scritti step del flusso JS

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// FLUSSO
+
+/* 
+-Chiedere l'età del passeggero
+-Chiedere i KM da percorrere
+-Calcolare il prezzo del biglietto: Km * 0,21€
+--SE età < 18
+---Applica 20% : prezzo * 0.80
+--INVECE SE età > 65
+---Applica 40% : prezzo * 0.60
+-Scrivi il prezzo
+*/
